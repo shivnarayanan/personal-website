2f3421ee0895c14aa92d47b5a51d7f960ac1aabb
shivnarayanan <shiv.narayanan@icloud.com>
2022-03-06 13:59:10 +0800

    commit

--- application.py
+++ application.py
@@ -4,7 +4,7 @@ application = Flask(__name__)
 
 @application.route('/')
 def helloworld():
-    return "Hello World! This is my first web application! I have removed some changes!"
+    return "Hello World! This is my first web application! I have made some changes!"
 
 if __name__ == "__main__":
     application.run(debug=True)
