d85ddc8f0216d36d4c4c4ef1f6b2d0f2aa745965
shivnarayanan <shiv.narayanan@icloud.com>
2022-03-27 23:44:24 +0800

    deployment#2

--- application.py
+++ application.py
@@ -3,8 +3,20 @@ from flask import Flask, render_template
 application = Flask(__name__)
 
 @application.route('/')
-def index():
+def start():
+    return home()
+
+@application.route('/home')
+def home():
     return render_template('home.html')
+
+@application.route('/about')
+def about():
+    return render_template('about.html')
+
+@application.route('/projects')
+def projects():
+    return render_template('projects.html')
 
 if __name__ == "__main__":
    application.run(debug=True)
